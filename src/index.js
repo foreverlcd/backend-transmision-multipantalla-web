4693a67265fec988b8aaf62957e9edddb49fa03f
foreverlcd <jhonjesusquispemachaca@gmail.com>
2025-09-07 19:32:35 -0500

    fixing error with env

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ io.use((socket, next) => {
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json());
